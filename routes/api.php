7c2e81fa84eca1e4984cbb0422f461ac7b015fa7
AlekseyMar <alekseyamart@gmail.com>
2019-12-29 02:19:38 +0200

    1создали роуты в api.php для users

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('/users')->group(function () {
+    Route::get('/', ['uses' => 'UserController@get']);
+        // UserController - назварие будущего контроллера
+        // get - назварие будущего метода (получить список всех юзеров)
+    Route::get('/{user_name}', ['uses' => 'UserController@detail']);//(получить одного по id)
+    Route::post('/', ['uses' => 'UserController@create']);
+    Route::delete('/{user_id}', ['uses' => 'UserController@delete'])-where(['user_id'=>'[0-9+]']);
+    Route::put('/{user_id}', ['uses' => 'UserController@update'])-where(['user_id'=>'[0-9+]']);
 });
