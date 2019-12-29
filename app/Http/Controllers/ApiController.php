949d0b3a52fb70b8a06cf3c4cf23315bc7fdf2c0
AlekseyMar <alekseyamart@gmail.com>
2019-12-29 02:29:43 +0200

    4 добавим код в метод get - ApiControllers.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,7 +3,12 @@
 	abstract class ApiControllers
 	{
 		protected $model;
-		public function get() {}
+		public function get() {
+            $limit = (int) $request->get('limit', 100);
+            $offset = (int) $request->get('offset', 0);
+            $result = $this->model->limit($limit)->offset($offset)->get();
+            return $this->sendResponse($result, 'OK',200);
+        }
 		public function detail(string $objectName = null) {} // метод получения конкретной записи по id
 		public function update(int $objectId) {}
 		public function delete(int $objectId) {}
